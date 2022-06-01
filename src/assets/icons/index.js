ad50bbc7414a02fffc9aaab65f2a6d3d3d1497e7
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-01 09:40:43 +0700

    Add list icon svg component into export default object

---
+++ src/assets/icons/index.js
@@ -0,0 +1,5 @@
+export { ReactComponent as CloseIcon } from './lib/Close Button.svg';
+export { ReactComponent as GridIcon } from './lib/grid.svg';
+export { ReactComponent as UserIcon } from './lib/user.svg';
+export { ReactComponent as LogoutIcon } from './lib/log-out.svg';
+export { ReactComponent as BookIcon } from './lib/book-open.svg';
