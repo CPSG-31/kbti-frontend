36242ab3a2b2cc1911e1905d54f1712e7b982de8
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-01 09:42:42 +0700

    Add navbar component and functionality drawer button into Admin layout.

--- src/components/Navbar/AdminNavbar/AdminNavbar.jsx
+++ src/components/Navbar/AdminNavbar/AdminNavbar.jsx
@@ -1,26 +1,65 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import navbarLogo from '../../../assets/images/logo/logo-yellow.png';
+import {
+  CloseIcon,
+  GridIcon,
+  UserIcon,
+  LogoutIcon,
+  BookIcon,
+} from '../../../assets/icons';
 
-const AdminNavbar = () => {
+import './AdminNavbar.scss';
+
+const AdminNavbar = ({ toggleClass, onClose }) => {
   const location = useLocation();
-
+  
+  const toggleClassHandler = () => {
+    onClose();
+  };
+  
   return (
-    <nav className="bg-warning">
-      <p>Navbar admin page</p>
-      <ul>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
+    <nav className={toggleClass ? 'nv nav-active' : 'nv'}>
+      <div className="nv__header d-flex justify-content-between align-items-center">
+        <Link to="/" ><img src={navbarLogo} alt="KBTI Logo" width={90} height={40}/></Link>
+        <button className="btn text-end d-lg-none d-block nv__close" type="button" onClick={toggleClassHandler}>
+          <CloseIcon />
+        </button>
+      </div>
+      <ul className="nv__list">
+        <li className="nv__list-item">
+          <NavLink to="/dashboard" className={({ isActive }) => isActive ? 'active' : ''}>
+            <GridIcon />
+            Dashboard
+          </NavLink>
         </li>
-        <li>
-          <Link to="/dashboard/definitions">Definitions</Link>
+        <li className="dropdown nv__list-item">
+            <button className="dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              <BookIcon />
+              Kelola Definisi
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li className="nv__list-item">
+                <NavLink to="/dashboard/definitions" className={({ isActive }) => isActive ? 'active' : ''}>Data Definisi</NavLink>
+              </li>
+              <li className="nv__list-item">
+                <NavLink to="/dashboard/review-definitions" className={({ isActive }) => isActive ? 'active' : ''}>Tinjau Definisi Baru</NavLink>
+              </li>
+              <li className="nv__list-item">
+                <NavLink to="/dashboard/deleted-definitions" className={({ isActive }) => isActive ? 'active' : ''}>Definisi yang dihapus</NavLink>
+              </li>
+            </ul>
         </li>
-        <li>
-          <Link to="/dashboard/review-definitions">Review Definitions</Link>
+        <li className="nv__list-item">
+          <NavLink to="/dashboard/users" className={({ isActive }) => isActive ? 'active' : ''}>
+            <UserIcon />
+            Kelola Pengguna
+          </NavLink>
         </li>
-        <li>
-          <Link to="/dashboard/deleted-definitions">Deleted Definition</Link>
-        </li>
-        <li>
-          <Link to="/dashboard/users">Manage Users</Link>
+        <li className="nv__list-item">
+          <button type="button">
+            <LogoutIcon />
+            Keluar
+          </button>
         </li>
       </ul>
     </nav>
--- src/layouts/Admin/Admin.jsx
+++ src/layouts/Admin/Admin.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { NavbarAdmin } from '../../components';
+import './Admin.scss';
 
 function AdminLayout() {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
+  const openNavbarHandler = () => setIsOpen(true);
+  
+  const closeNavbarHandler = () => setIsOpen(false);
+  
   return (
     <>
-      <header className="p-3">
-        <NavbarAdmin />
-      </header>
-      <main>
-        {location.pathname === "/dashboard" ? <p>Statistic</p> : <Outlet />}
-      </main>
-      <footer>
-        This is footer
-      </footer>
+      <div className="admin-layout d-flex">
+        <header>
+          <NavbarAdmin toggleClass={isOpen} onClose={closeNavbarHandler}/>
+        </header>
+        <div className="w-100 d-flex flex-column admin-layout__body">
+          <main>
+            {!isOpen && (
+              <div className="nav__drawer-container d-block d-lg-none">
+                <button type="button" className="btn nav__drawer-container-item" onClick={openNavbarHandler}>☰</button>
+              </div>
+            )}
+            {location.pathname === '/dashboard' ? <p>Statistic</p> : <Outlet />}
+          </main>
+          <footer>
+            This is footer
+          </footer>
+        </div>
+      </div>
     </>
   );
 }
