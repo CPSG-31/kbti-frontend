6b3d0764acb0e73d0a6a4419dad416351f50ebd9
Tryo Asnafi <tryoasnafi@gmail.com>
2022-06-05 23:04:15 +0700

    add register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,45 @@
+import '../../index.css';
+import '../../styles/global.css';
+import '../../styles/Auth.css';
+
 const Register = () => {
   return (
-    <div>
-      This is Register page
-    </div>
+    <>
+      <div className="container auth-container">
+        <div className="h-100 d-flex flex-column align-items-center justify-content-center">
+          <form className="col-10 col-md-6 col-lg-4 py-4">
+            <h1 className="form__title mb-5">Daftar</h1>
+            <div className="row mb-3">
+              <label htmlFor="usernameInput" className="form-label">
+                Username
+                <input type="text" name="username" id="usernameInput" className="form-control form__input mt-1" placeholder="Username" required minLength={3} />
+              </label>
+            </div>
+            <div className="row mb-3">
+              <label htmlFor="emailInput" className="form-label">
+                Email
+                <input type="email" name="email" id="emailInput" className="form-control form__input mt-1" placeholder="Email Anda" required />
+              </label>
+            </div>
+            <div className="row mb-3">
+              <label htmlFor="passwordInput" className="form-label">
+                Password
+                <input type="password" name="password" id="passwordInput" className="form-control form__input mt-1" placeholder="Masukkan Password" minLength={8} required />
+              </label>
+            </div>
+            <div className="row mb-3 mt-4">
+              <input type="submit" className="mx-auto btn btn-kbti rounded-pill" value="Daftar" />
+              <p className="text-center mt-3">
+                Sudah punya akun?
+                {' '}
+                <a href="#" className="kbti-link pl-2">Masuk</a>
+              </p>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
