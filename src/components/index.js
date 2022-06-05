e6cae9f8eb8d5448071718adadbb2b08c97274f6
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-05 17:06:06 +0700

    Add list component into export default object

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as NavbarPublic } from './Navbar/PublicNavbar/PublicNavbar';
 export { default as NavbarAdmin } from './Navbar/AdminNavbar/AdminNavbar';
 export { default as Error } from './Error/Error';
+export { default as Table } from './Table/Table';
+export { default as TableDataDefinition } from './TableDataDefinition/TableDataDefinition';
+export { default as Paginate } from './Paginate/Paginate';
