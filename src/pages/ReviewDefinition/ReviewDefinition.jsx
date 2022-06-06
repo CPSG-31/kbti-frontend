e69fdb7fc8ebd8c7a9ee298895e3766f054164c8
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-06 08:01:16 +0700

    Add Review definition component page and functionality

--- src/pages/ReviewDefinition/ReviewDefinition.jsx
+++ src/pages/ReviewDefinition/ReviewDefinition.jsx
@@ -1,9 +1,62 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+  Paginate,
+  Table, TableDataReviewDefinition,
+} from '../../components';
+
+import './ReviewDefinition.scss';
+
 const ReviewDefinition = () => {
+  const [items, setItems] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+  
+  const fetchData = async (queryCurrentPage = 1) => {
+    const response = await axios.get(`http://localhost:3001/definitions?_page=${queryCurrentPage}&_limit=10`);
+    const totalData = response.headers['x-total-count'];
+    const totalPages = Math.ceil(totalData / 10);
+    
+    setPageCount(totalPages);
+    setTotalItems(totalData);
+    return response.data;
+  };
+  
+  useEffect(() => {
+    const firstTimeFetchData = async () => {
+      const data = await fetchData();
+      setItems(data);
+    };
+    
+    firstTimeFetchData();
+  }, []);
+  
+  const paginateChangeHandler = async (page) => {
+    const currentPagePaginate = page.selected + 1;
+    
+    const fetchNewData = await fetchData(currentPagePaginate);
+    setItems(fetchNewData);
+    setCurrentPage(currentPagePaginate);
+  };
+  
   return (
-    <div>
-      This is Review Definition page
-    </div>
-  )
+    <section className="review__definition-admin">
+      <h1>Tinjau Definisi Baru</h1>
+      
+      <Table
+        items={items}
+        currentPage={currentPage}
+        totalItems={totalItems}
+        component={<TableDataReviewDefinition items={items} currentPage={currentPage}/>}
+      />
+      
+      <Paginate
+        pageCount={pageCount}
+        paginateChangeHandler={paginateChangeHandler}
+      />
+    </section>
+  );
 };
 
 export default ReviewDefinition;
