e665ed605263e6f25d31b396a1969b3c848c6564
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-06 08:31:03 +0700

    Add Detail review definition page component

--- src/pages/ReviewDetailDefinition/ReviewDetailDefinition.jsx
+++ src/pages/ReviewDetailDefinition/ReviewDetailDefinition.jsx
@@ -1,13 +1,41 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import './ReviewDetailDefinition.scss';
 
 const ReviewDetailDefinition = () => {
   const { idDefinition } = useParams();
 
   return (
-    <div>
-      This is Review Detail Definition page with idDefinition : {idDefinition}
-    </div>
-  )
+    <section className="form-admin">
+      <h1 className="text-center">Tinjau Definisi Baru</h1>
+  
+      <form className="form-admin__container">
+        <div className="form-admin__group">
+          <label htmlFor="term">Istilah</label>
+          <input type="text" className="form-control" id="term" value="Istilah" readOnly/>
+        </div>
+        <div className="form-admin__group">
+          <label htmlFor="definition">Definisi</label>
+          <textarea
+            className="form-control"
+            id="definition"
+            value="komputer adalah alat yang dipakai untuk mengolah data menurut prosedur yang telah dirumuskan. komputer adalah suatu perangkat keras yang sangat berkaitan dengan teknologi. komputer mampu membantu berbagai pekerjaan manusia. Kata komputer pada awalnya dipergunakan untuk menggambarkan orang yang perkerjaannya melakukan perhitungan aritmetika, dengan atau tanpa alat bantu, tetapi arti kata ini kemudian dipindahkan kepada mesin itu sendiri."
+            rows="9"
+            readOnly
+          />
+        </div>
+        <div className="form-admin__group">
+          <label htmlFor="category">Kategori</label>
+          <select className="form-control" id="category" readOnly>
+            <option>Jaringan</option>
+          </select>
+        </div>
+        <div className="form-admin__group">
+          <button className="btn form-admin__button-approve">Approve</button>
+          <button className="btn form-admin__button-reject">Reject</button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default ReviewDetailDefinition;
