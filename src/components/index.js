24461af146dea2c46c6c65791ec0c5834f748bbf
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-06 08:48:28 +0700

    Add table data deleted definition component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ export { default as Error } from './Error/Error';
 export { default as Table } from './Table/Table';
 export { default as TableDataDefinition } from './TableDataDefinition/TableDataDefinition';
 export { default as TableDataReviewDefinition } from './TableDataReviewDefinition/TableDataReviewDefinition';
+export { default as TableDataDeletedDefinition } from './TableDataDeletedDefinition/TableDataDeletedDefinition';
 export { default as Paginate } from './Paginate/Paginate';
 export { default as Footer } from './Footer/Footer';
