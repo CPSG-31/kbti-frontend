150e8e7f3ee555b5ba77bb3f46d6219117af7011
Zahra <zhraulia16@gmail.com>
2022-06-10 19:54:02 +0700

    add functional with static data

--- src/pages/DashboardUser/DashboardUser.jsx
+++ src/pages/DashboardUser/DashboardUser.jsx
@@ -1,17 +1,106 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/button-has-type */
 import './DashboardUser.css';
+import { useState } from 'react';
 import { UserTermCard } from '../../components';
 import { PlusSvg, InfoSvg } from '../../icons';
+import emptyListImg from '../../assets/images/empty-list.png';
+import STATUS from '../../globals/const';
 
 const DashboardUser = () => {
+  const userTerms = [
+    {
+      status: 'pending',
+      term: 'Blockchain',
+      date: '22 Mei 2022',
+      shortDescription:
+        'Teknologi yang digunakan sebagai sistem penyimpanan atau bank data secara digital yang terhubung dengan kriptografi.',
+    },
+    {
+      status: 'approved',
+      term: 'Blockchain',
+      date: '22 Mei 2022',
+      shortDescription:
+        'Teknologi yang digunakan sebagai sistem penyimpanan atau bank data secara digital yang terhubung dengan kriptografi.',
+    },
+    {
+      status: 'rejected',
+      term: 'Blockchain',
+      date: '22 Mei 2022',
+      shortDescription:
+        'Teknologi yang digunakan sebagai sistem penyimpanan atau bank data secara digital yang terhubung dengan kriptografi.',
+    },
+    // <UserTermCard
+    //   status="rejected"
+    //   term="Blockchain"
+    //   date="22 Mei 2022"
+    //   shortDescription="Teknologi yang digunakan sebagai sistem penyimpanan atau bank data secara digital yang terhubung dengan kriptografi."
+    // />,
+  ];
+  const [termList, setTermList] = useState(userTerms);
+
+  const approvedTerms = userTerms.filter(
+    (userTerm) => userTerm.status === STATUS.approved,
+  );
+  const pendingTerms = userTerms.filter(
+    (userTerm) => userTerm.status === STATUS.pending,
+  );
+  const rejectedTerms = userTerms.filter(
+    (userTerm) => userTerm.status === STATUS.rejected,
+  );
+
+
+  const createTermCardElements = (termData) => termData.map(({ status, term, date, shortDescription }) => (
+    <UserTermCard
+      status={status}
+      term={term}
+      date={date}
+      shortDescription={shortDescription}
+    />
+  ));
+  const TermCardElements = createTermCardElements(termList);
+
+  const approvedButtonHandler = () => {
+    setTermList(approvedTerms);
+    createTermCardElements(approvedTerms);
+  };
+
+  const pendingButtonHandler = () => {
+    setTermList(pendingTerms);
+    createTermCardElements(pendingTerms);
+  };
+
+  const rejecteddButtonHandler = () => {
+    setTermList(rejectedTerms);
+    createTermCardElements(rejectedTerms);
+  };
+
+  const seeAllButtonHandler = () => {
+    setTermList(userTerms);
+    createTermCardElements(userTerms);
+  };
+
+  const emptyListState = (
+    <div className="empty-list d-flex flex-column justify-content-center">
+      <p className="empty-list-text text-center">
+        Belum ada istilah yang kamu definisikan
+      </p>
+      <img
+        className="empty-list-img mx-auto"
+        src={emptyListImg}
+        alt="empty user definition list"
+      />
+    </div>
+  );
+
+
   return (
     <div className="dashboard-user">
       <div className="row gx-5 row-cols-2">
         <div className="dashboard__aside-container col-12 col-lg-4 mt-4 mb-4">
           <div className="dashboard__user-action d-flex">
             <a
-              className="add-term__button btn btn-primary rounded-pill pt4 my-1 me-2 align-middle lh-lg"
+              className="btn-kbti btn w-100 rounded-pill pt4 my-1 me-2 align-middle lh-lg"
               href="#"
               role="button"
             >
@@ -33,42 +122,65 @@ const DashboardUser = () => {
               </h6>
               <div className="status-container mt-2 container">
                 <div className="row px-3">
-                  <a href="#" className="card-link col-4 m-0 p-0 d-flex flex-column">
-                    <span className="status-count text-center">10</span>
-                    <span className="status-text text-center">disetujui</span>
-                  </a>
-                  <a href="#" className="card-link col-4 m-0 p-0 d-flex flex-column ">
-                    <span className="status-count text-center">2</span>
-                    <span className="status-text text-center">sedang ditinjau</span>
-                  </a>
-                  <a href="#" className="card-link col-4 m-0 p-0 d-flex flex-column">
-                    <span className="status-count text-center">3</span>
-                    <span className="status-text text-center">ditolak</span>
-                  </a>
+                  <button
+                    type="button"
+                    onClick={() => approvedButtonHandler()}
+                    className="btn btn-link col-4 m-0 p-0 d-flex flex-column"
+                  >
+                    <span className="status-count text-center w-100">
+                    {approvedTerms.length}
+                    </span>
+                    <span className="status-text text-center w-100">
+                      disetujui
+                    </span>
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => pendingButtonHandler()}
+                    className="btn btn-link col-4 m-0 p-0 d-flex flex-column"
+                  >
+                    <span className="status-count text-center w-100">
+                      {pendingTerms.length}
+                    </span>
+                    <span className="status-text text-center w-100">
+                      sedang ditinjau
+                    </span>
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => rejecteddButtonHandler()}
+                    className="btn btn-link col-4 m-0 p-0 d-flex flex-column"
+                  >
+                    <span className="status-count text-center w-100">
+                      {rejectedTerms.length}
+                    </span>
+                    <span className="status-text text-center w-100">
+                      ditolak
+                    </span>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="dashboard__terms-container col-12 col-lg-8 mt-4">
-          <UserTermCard
-            status="pending"
-            term="Blockchain"
-            date="22 Mei 2022"
-            shortDescription="Teknologi yang digunakan sebagai sistem penyimpanan atau bank data secara digital yang terhubung dengan kriptografi."
-          />
-          <UserTermCard
-            status="approved"
-            term="Blockchain"
-            date="22 Mei 2022"
-            shortDescription="Teknologi yang digunakan sebagai sistem penyimpanan atau bank data secara digital yang terhubung dengan kriptografi."
-          />
-          <UserTermCard
-            status="rejected"
-            term="Blockchain"
-            date="22 Mei 2022"
-            shortDescription="Teknologi yang digunakan sebagai sistem penyimpanan atau bank data secara digital yang terhubung dengan kriptografi."
-          />
+          {termList.length !== userTerms.length 
+            ? (
+            <div
+              className="dashboard__terms-header d-flex justify-content-end"
+            >
+              <button
+                type="button" 
+                onClick={() => seeAllButtonHandler()}
+                className="btn btn-link mb-4"
+              >
+              Lihat semua
+              </button>
+            </div>
+            ) : ''}
+          {TermCardElements.length > 0
+            ? TermCardElements
+            : emptyListState}
         </div>
       </div>
     </div>
