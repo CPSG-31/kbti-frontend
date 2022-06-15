51f0280ed5aff0c0e16e8f7c8bceaf6a01fc748d
Zahra <zhraulia16@gmail.com>
2022-06-15 21:51:14 +0700

    add functional

--- src/components/PublicNavbar/PublicNavbar.jsx
+++ src/components/PublicNavbar/PublicNavbar.jsx
@@ -1,15 +1,50 @@
+/* eslint-disable react/no-this-in-sfc */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/button-has-type */
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import useAuth from '../../hooks/useAuth';
 import './PublicNavbar.css';
 import Logo from '../../assets/images/logo/logo-yellow.png';
 
 function PublicNavbar() {
   const { isLoggedIn, role_id: roleId, token, logout } = useAuth();
+  const [categories, setCategories] = useState([]);
   const role = roleId === 1 ? 'admin' : 'user';
-
   let actionNavbar;
+
+  const fetchCategories = async () => {
+    const response = await axios.get('https://kbti-api.herokuapp.com/categories'); 
+    const categoryData = response.data;
+
+    return categoryData;
+  };
+  
+
+  useEffect(() => {
+    const firstTimeFetchData = async () => {
+      const response = await fetchCategories();
+      setCategories(response.data);
+    };
+    
+    firstTimeFetchData();
+  }, []);
+
+  
+  const categoryElements = (categoryList) => categoryList && categoryList.map((categoryItem) => {
+    return (
+      <li key={categoryItem.id}>
+        <a className="dropdown-item category-item" href={`/definitions?categoryId=${categoryItem.id}`}>
+          {categoryItem.category}
+        </a>
+      </li>
+    );
+  });
+
+  // const a = categoryElementList && console.log(categoryElementList);
+
+
   if ((!token && !isLoggedIn) || (role !== 'admin' && role !== 'user')) {
     actionNavbar = (
       <>
@@ -82,6 +117,7 @@ function PublicNavbar() {
                           type="button"
                           className="col-1 btn btn-outline-warning rounded-circle fw-bold"
                           key={index}
+                          onClick={() => { window.location.href = `/search?q=${letter}`; }}
                         >
                           {letter}
                         </button>
@@ -105,26 +141,7 @@ function PublicNavbar() {
                   className="dropdown-menu"
                   aria-labelledby="navbarDropdownMenuLink"
                 >
-                  <li>
-                    <a className="dropdown-item category-item" href="#">
-                      Internet
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item category-item" href="#">
-                      Pemrograman
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item category-item" href="#">
-                      Jaringan
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item category-item" href="#">
-                      dll.
-                    </a>
-                  </li>
+                {categoryElements(categories)}
                 </ul>
               </li>
             </div>
