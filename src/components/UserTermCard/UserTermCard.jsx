2413fad9f2e65fbf0f7dbd7e95ac8474f8ca297e
Zahra <zhraulia16@gmail.com>
2022-06-15 21:51:51 +0700

    add functional

--- src/components/UserTermCard/UserTermCard.jsx
+++ src/components/UserTermCard/UserTermCard.jsx
@@ -1,21 +1,44 @@
-/* eslint-disable eqeqeq */
+/* eslint-disable react/destructuring-assignment */
+import axios from 'axios';
 import './UserTermCard.css';
-import {
- EditSvg, ThumbsUpSvg, ThumbsDownSvg, TrashSvg, 
-} from '../../icons';
+import { EditSvg, ThumbsUpSvg, ThumbsDownSvg, TrashSvg } from '../../icons';
 import STATUS from '../../globals/const';
 import formatDate from '../../utils/formatDate';
+import useAuth from '../../hooks/useAuth';
 
-const UserTermCard = ({
-  id, statusDefinition, term, date, definition, upvote, downvote,
-}) => {
+const UserTermCard = ({ dataDefinition, deleteButtonHandler }) => {
   let statusElement = '';
+  const { token } = useAuth();
+  const {
+    id,
+    term,
+    category,
+    statusDefinition,
+    definition,
+    down_votes: downvote,
+    up_votes: upvote,
+    updatedAt: date,
+  } = dataDefinition;
 
+  // const handleRemove = (termId) => {
+  //   axios
+  //     .delete(`https://kbti-api.herokuapp.com/definitions/${termId}`, { headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     } });
+  //     // .then((res) => {
+  //     //   setTermList(userData.definitions);
+  //     // });
+  //     // .catch((err) => {
+  //     //   console.log(err);
+  //     // });
+  // };
+
+  // category, statusDefinition, term, updatedAt, definition, up_votes = null, down_votes = null
   if (statusDefinition === STATUS.deleted) {
     return '';
   }
 
-  if (statusDefinition == STATUS.pending) {
+  if (statusDefinition === STATUS.pending) {
     statusElement = (
       <div className="term__status-pending rounded-pill">
         <p className="term___status-text">{STATUS.pending}</p>
@@ -51,7 +74,6 @@ const UserTermCard = ({
     );
   }
 
-  console.log(date);
   return (
     <div className="card mb-3">
       <div className="card-body">
@@ -60,7 +82,16 @@ const UserTermCard = ({
         </div>
         <h2 className="card-title mb-0 mt-4">{term}</h2>
         <div className="term__info">
-          <small className="text-muted">{formatDate(date)}</small>
+          <div className="term__info mb-2">
+            <p
+              href="#"
+              className="term__category d-inline-block mb-1 p-1 rounded"
+            >
+              {category}
+            </p>
+            <span className="mx-1 text-muted">&#8226;</span>
+            <small className="text-muted">{formatDate(date)}</small>
+          </div>
         </div>
         <p className="card-text mt-2 mb-0">{definition}</p>
         <a href="#" className="">
@@ -70,7 +101,7 @@ const UserTermCard = ({
           <button type="button" className="term__action-button btn">
             <EditSvg />
           </button>
-          <button type="button" className="term__action-button btn">
+          <button onClick={() => deleteButtonHandler(id)} type="button" className="term__action-button btn">
             <TrashSvg />
           </button>
         </div>
