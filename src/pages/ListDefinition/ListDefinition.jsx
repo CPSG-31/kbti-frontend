6b9b3e5797af6dfc43a9a913c3aa817d9e843876
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-15 21:15:11 +0700

    Add delete definition feature

--- src/pages/ListDefinition/ListDefinition.jsx
+++ src/pages/ListDefinition/ListDefinition.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { PlusSvg } from '../../assets/icons';
@@ -9,39 +9,75 @@ import {
 } from '../../components';
 
 import './ListDefinition.scss';
+import API_ENDPOINT from '../../globals/apiEndpoint';
+import useAuth from '../../hooks/useAuth';
 
 const ListDefinition = () => {
-  const [items, setItems] = useState([]);
-  const [pageCount, setPageCount] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalItems, setTotalItems] = useState(0);
+  const [data, setData] = useState(null);
+  const { token } = useAuth();
   
-  const fetchData = async (queryCurrentPage = 1) => {
-    const response = await axios.get(`http://localhost:3001/definitions?_page=${queryCurrentPage}&_limit=10`);
-    const totalData = response.headers['x-total-count'];
-    const totalPages = Math.ceil(totalData / 10);
-    
-    setPageCount(totalPages);
-    setTotalItems(totalData);
-    return response.data;
-  };
-  
-  useEffect(() => {
-    const firstTimeFetchData = async () => {
-      const data = await fetchData();
-      setItems(data);
-    };
-    
-    firstTimeFetchData();
+  const fetchData = useCallback(async (queryCurrentPage = 1) => {
+    try {
+      const response = await axios.get(API_ENDPOINT.ADMIN_DEFINITIONS(queryCurrentPage), {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      
+      await setData(response.data);
+    } catch (error) {
+      console.warn(error);
+    }
   }, []);
   
   const paginateChangeHandler = async (page) => {
     const currentPagePaginate = page.selected + 1;
     
-    const fetchNewData = await fetchData(currentPagePaginate);
-    setItems(fetchNewData);
+    fetchData(currentPagePaginate);
     setCurrentPage(currentPagePaginate);
   };
+  
+  const deleteDefinitionHandler = async (id) => {
+    await Swal.fire({
+      title: 'Apakah Anda Yakin?',
+      text: 'Ingin menghapus definisi ini!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Hapus',
+      cancelButtonText: 'Batal',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await axios.delete(API_ENDPOINT.DELETE_DEFINITION(id), {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          Swal.fire({
+            title: 'Berhasil!',
+            text: 'Definisi berhasil dihapus!',
+            icon: 'success',
+            timer: 2000,
+          });
+          fetchData();
+        } catch (error) {
+          Swal.fire({
+            title: 'Gagal!',
+            text: `${error.response.data.message}!`,
+            icon: 'error',
+            timer: 2000,
+          });
+        }
+      }
+    });
+  };
+  
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   
   return (
     <section className="list__definition-admin">
@@ -50,14 +86,17 @@ const ListDefinition = () => {
         <PlusSvg />
         Tambah Definisi
       </Link>
-      
-      <Table
-        items={items}
-        currentPage={currentPage}
-        totalItems={totalItems}
-        component={<TableDataDefinition items={items} currentPage={currentPage}/>}
-      />
-      <Paginate pageCount={pageCount} paginateChangeHandler={paginateChangeHandler}/>
+      {data && (
+        <>
+          <Table
+            items={data}
+            currentPage={currentPage}
+            totalItems={data.meta.total}
+            component={<TableDataDefinition items={data} currentPage={currentPage} onDeleteDefinition={deleteDefinitionHandler}/>}
+          />
+          <Paginate pageCount={Math.ceil(data.meta.total / 10)} paginateChangeHandler={paginateChangeHandler}/>
+        </>
+      )}
     </section>
   );
 };
