8f612b7a0173adc443a188cf617bb22d5aea1a6d
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-16 11:10:17 +0700

    Update with axios request  and add sweet alert for response

--- src/pages/ReviewDefinition/ReviewDefinition.jsx
+++ src/pages/ReviewDefinition/ReviewDefinition.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import {
   Paginate,
   Table, TableDataReviewDefinition,
 } from '../../components';
+import API_ENDPOINT from '../../globals/apiEndpoint';
+import useAuth from '../../hooks/useAuth';
 
 import './ReviewDefinition.scss';
 
@@ -11,50 +13,58 @@ const ReviewDefinition = () => {
   const [items, setItems] = useState([]);
   const [pageCount, setPageCount] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
+  const [data, setData] = useState(null);
   const [totalItems, setTotalItems] = useState(0);
+  const { token } = useAuth();
   
-  const fetchData = async (queryCurrentPage = 1) => {
-    const response = await axios.get(`http://localhost:3001/definitions?_page=${queryCurrentPage}&_limit=10`);
-    const totalData = response.headers['x-total-count'];
-    const totalPages = Math.ceil(totalData / 10);
+  const fetchData = useCallback(async (queryCurrentPage = 1) => {
+    try {
+      const response = await axios.get(API_ENDPOINT.ADMIN_DEFINITIONS_REVIEW(queryCurrentPage), {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
     
-    setPageCount(totalPages);
-    setTotalItems(totalData);
-    return response.data;
-  };
-  
-  useEffect(() => {
-    const firstTimeFetchData = async () => {
-      const data = await fetchData();
-      setItems(data);
-    };
-    
-    firstTimeFetchData();
+      await setData(response.data);
+    } catch (error) {
+      console.warn(error);
+    }
   }, []);
   
   const paginateChangeHandler = async (page) => {
     const currentPagePaginate = page.selected + 1;
     
-    const fetchNewData = await fetchData(currentPagePaginate);
-    setItems(fetchNewData);
+    // const fetchNewData = await fetchData(currentPagePaginate);
+    // setItems(fetchNewData);
+    // setCurrentPage(currentPagePaginate);
+    await fetchData(currentPagePaginate);
     setCurrentPage(currentPagePaginate);
   };
+  
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+  
+  
   
   return (
     <section className="review__definition-admin">
       <h1>Tinjau Definisi Baru</h1>
-      
-      <Table
-        items={items}
-        currentPage={currentPage}
-        totalItems={totalItems}
-        component={<TableDataReviewDefinition items={items} currentPage={currentPage}/>}
-      />
-      
-      <Paginate
-        pageCount={pageCount}
-        paginateChangeHandler={paginateChangeHandler}
-      />
+      {data && (
+        <>
+          <Table
+            items={data}
+            currentPage={currentPage}
+            totalItems={data.meta.total}
+            component={<TableDataReviewDefinition items={data} currentPage={currentPage}/>}
+          />
+  
+          <Paginate
+            pageCount={Math.ceil(data.meta.total / 10)}
+            paginateChangeHandler={paginateChangeHandler}
+          />
+        </>
+      )}
     </section>
   );
 };
