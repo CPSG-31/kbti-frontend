aae76b81f92cf6ccd949dde56dfc331a9d78da80
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-21 14:23:45 +0700

    Add check token functionality

--- src/globals/apiEndpoint.js
+++ src/globals/apiEndpoint.js
@@ -3,6 +3,7 @@ import CONFIG from './config';
 const API_ENDPOINT = {
   LOGIN: `${CONFIG.BASE_URL}/login`,
   REGISTER: `${CONFIG.BASE_URL}/register`,
+  CHECK_TOKEN: `${CONFIG.BASE_URL}/auth/token`,
   DELETE_DEFINITION: (id) => `${CONFIG.BASE_URL}/definitions/${id}`,
   CATEGORY: `${CONFIG.BASE_URL}/categories`,
   CREATE_DEFINITION: `${CONFIG.BASE_URL}/definitions`,
