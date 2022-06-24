b5e9f7a687fa3cba7350cc2eaac5d88326cbdce5
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-06-24 07:57:04 +0700

    Add configuration for manifest and workbox plugin

---
+++ config-overrides.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { override, addWebpackPlugin } = require('customize-cra');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
+module.exports = override(
+  addWebpackPlugin(
+    new WebpackPwaManifest({
+      name: 'KBTI',
+      short_name: 'KBTI',
+      description: 'KBTI merupakan aplikasi untuk pencarian istilah tentang IT',
+      start_url: './',
+      filename: 'manifest.json',
+      display: 'standalone',
+      background_color: '#ffffff',
+      orientation: 'portrait',
+      theme_color: '#2196f3',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/assets/images/logo/iconKBTI.png'),
+          type: 'image/png',
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: 'static/media',
+          purpose: 'maskable',
+        },
+        {
+          src: path.resolve('src/assets/images/logo/iconKBTI.png'),
+          type: 'image/png',
+          size: '144x144',
+          destination: 'static/media',
+          purpose: 'any',
+        },
+      ],
+    }),
+  ),
+  addWebpackPlugin(
+    new InjectManifest({
+      maximumFileSizeToCacheInBytes: 5000000,
+      swSrc: './src/service-worker.js',
+      swDest: process.env.NODE_ENV === 'development' ? './service-worker.js' : path.resolve('service-worker.js'),
+    }),
+  ),
+);
