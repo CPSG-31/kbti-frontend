48c95de732bd0ef2af918414a34925d5cb4a87d5
Zahra <zhraulia16@gmail.com>
2022-06-24 20:59:13 +0700

    add error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,11 +1,13 @@
+import NotFoundImg from '../../assets/images/not-found-illustration.png';
 
 function ErrorPage() {
   return (
-    <>
-      <p>
-        Error page not found
-      </p>
-    </>
+    <div className="homepage h-100 d-flex justify-content-center">
+      <div className="container__not-found align-middle my-auto">
+        <img src={NotFoundImg} alt="page not found" />
+        <p className="text-center text-dark fs-4 mt-2">Error page not found</p>
+      </div>
+    </div>
   );
 }
 
